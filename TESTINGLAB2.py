37cc3c8589d733b312e7c40e74daaa041005d5ac
timmesh1517@gmail.com <timmesh1517@gmail.com>
2024-11-07 08:05:01 +0530

    Initial commit

---
+++ TESTINGLAB2.py
@@ -0,0 +1,14 @@
+from socks import PRINTABLE_PROXY_TYPES
+print("welcome to eox")
+
+#variables
+import keyword
+print(keyword.kwlist)
+
+age=65
+age=90
+print(age)
+
+age=29
+pi=3.13
+name="saithimmesh"
---
+++ testing academy lab1.py
@@ -0,0 +1,10 @@
+print("hello eox vantage")
+print("SAITHIMMESH", 152025)
+print("SAITHIMMESH", 152025, sep='*',)
+print("SAITHIMMESH", 152025, sep='_',)
+
+
+
+
+
+
